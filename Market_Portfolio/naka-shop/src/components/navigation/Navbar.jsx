fc0a4968535de2dc76b2d475321e283cf5c6ecf4
Naka-studio <bayanaka.real@gmail.com>
2026-09-02 16:04:24 +0800

    feat: add Market_Portfolio

---
+++ Market_Portfolio/naka-shop/src/components/navigation/Navbar.jsx
@@ -0,0 +1,26 @@
+
+const Navbar = () => {
+  return (
+    <div className="navbar py-7 flex items-center justify-between">
+      <div className="logo">
+      	<h1>Testing</h1>
+      </div>
+			<ul className="menu flex items-center gap-10">
+				<li>
+				  <a href="#">menu</a>
+				</li>
+				<li>
+				  <a href="#">menu</a>
+				</li>
+				<li>
+				  <a href="#">menu</a>
+				</li>
+				<li>
+				  <a href="#">menu</a>
+				</li>
+			</ul>
+    </div>
+  )
+}
+
+export default Navbar
